067854f8ce1ae5144f8957364cca49a835899abb
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-05-22 17:19:23 -0400

    Upgrade LDAP auth backends
    * Now both WmuAuth and CaeAuth user creation will ping main campus
    for data (previously, only Wmu auth did this)
    * Created "safer" get_user_attribute method for WmuAuth. Their
    LDAP records are inconsistent so this was necessary.
    * Used above method to significantly improve reliablity of WmuAuth.
    * Improve handling for user entering "bad characters" in username.
    * Reduce Auth Class load times (and by extension, load times of all
    pages on the site that require permission checks).

--- settings/backends.py
+++ settings/backends.py
@@ -8,6 +8,7 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group, Permission
 from django.core.exceptions import ValidationError
 from django.core.validators import validate_email
+from phonenumber_field.phonenumber import PhoneNumber
 
 from cae_home import models
 from settings import simple_ldap_lib
@@ -22,9 +23,20 @@ class CaeAuthBackend(object):
     Custom authentication through the CAE Center LDAP.
     """
     def __init__(self):
+        """
+        Note: "check_credentials" value is set to False, because otherwise it will ping the LDAP server to verify
+            the "master" credentials every time this Auth Backend is called.
+
+            When set to active in settings, this backend is used every time a page needs to check permissions. So
+            enabling "check_credentials" would potentially add a lot of extra time to each page load.
+        """
         self.ldap_lib = simple_ldap_lib.SimpleLdap()
         self.ldap_lib.set_host(settings.CAE_LDAP['host'])
-        self.ldap_lib.set_master_account(settings.CAE_LDAP['login_dn'], settings.CAE_LDAP['login_password'])
+        self.ldap_lib.set_master_account(
+            settings.CAE_LDAP['login_dn'],
+            settings.CAE_LDAP['login_password'],
+            check_credentials=False,
+        )
         self.ldap_lib.set_search_base(settings.CAE_LDAP['user_search_base'])
 
     #region User Auth
@@ -41,6 +53,10 @@ class CaeAuthBackend(object):
 
         # Check what format username was provided as.
         user_id = self._parse_username(username)
+
+        if user_id is None:
+            logger.info('Auth Backend: CAE user login failed.')
+            return None
 
         try:
             # Attempt to get user object.
@@ -164,64 +180,65 @@ class CaeAuthBackend(object):
             logger.info('Auth Backend: Attempting to create new user model...')
 
         # Connect to server and pull user's full info.
-        self.ldap_lib.bind_server()
-        ldap_user = self.ldap_lib.search(
-            search_filter='(uid={0})'.format(uid),
-            attributes=['uid', 'givenName', 'sn',]
-        )
-        self.ldap_lib.unbind_server()
-
+        ldap_user_info = self.get_ldap_user_info(uid, attributes=['uid', 'givenName', 'sn',])
         ldap_user_groups = self.get_ldap_user_groups(uid)
 
         # Create new user.
-        model_user, created = models.User.objects.get_or_create(username=uid)
+        login_user, created = models.User.objects.get_or_create(username=uid)
 
         # Double check that user was created. If not, then duplicate user ids exist somehow. Error.
         if created:
             # Set password.
-            model_user.set_password(password)
-            model_user.save()
+            login_user.set_password(password)
+            login_user.save()
 
             # Set general user values.
-            model_user.email = '{0}@wmich.edu'.format(uid)
-            model_user.first_name = ldap_user['givenName'][0].strip()
-            model_user.last_name = ldap_user['sn'][0].strip()
+            login_user.email = '{0}@wmich.edu'.format(uid)
+            login_user.first_name = ldap_user_info['givenName'][0].strip()
+            login_user.last_name = ldap_user_info['sn'][0].strip()
 
             # Save model in case of error.
-            model_user.save()
+            login_user.save()
             if settings.AUTH_BACKEND_DEBUG:
                 logger.info('Auth Backend: Created user new user model {0}. Now setting groups...'.format(uid))
 
             # Set user group types.
             if ldap_user_groups['director']:
-                model_user.groups.add(Group.object.get(name='CAE Director'))
+                login_user.groups.add(Group.object.get(name='CAE Director'))
                 if settings.AUTH_BACKEND_DEBUG:
                     logger.info('Auth Backend: Added user to CAE Director group.')
             if ldap_user_groups['attendant']:
-                model_user.groups.add(Group.objects.get(name='CAE Attendant'))
+                login_user.groups.add(Group.objects.get(name='CAE Attendant'))
                 if settings.AUTH_BACKEND_DEBUG:
                     logger.info('Auth Backend: Added user to CAE Attendant group.')
             if ldap_user_groups['admin']:
-                model_user.groups.add(Group.objects.get(name='CAE Admin'))
+                login_user.groups.add(Group.objects.get(name='CAE Admin'))
                 if settings.AUTH_BACKEND_DEBUG:
                     logger.info('Auth Backend: Added user to CAE Admin group.')
             if ldap_user_groups['programmer']:
-                model_user.groups.add(Group.objects.get(name='CAE Programmer'))
-                model_user.is_staff = True
+                login_user.groups.add(Group.objects.get(name='CAE Programmer'))
+                login_user.is_staff = True
                 if settings.AUTH_BACKEND_DEBUG:
                     logger.info('Auth Backend: Added user to CAE Programmer group.')
 
             # Save model.
-            model_user.save()
+            login_user.save()
             if settings.AUTH_BACKEND_DEBUG:
-                logger.info('Auth Backend: User groups set. User creation complete.'.format(uid))
+                logger.info('Auth Backend: CAE Center User groups set.'.format(uid))
+                logger.info('Auth Backend: Attempting to get Main Campus user info...')
+
+            wmu_ldap = WmuAuthBackend()
+            wmu_ldap.update_or_create_wmu_user_model(uid)
+
+            if settings.AUTH_BACKEND_DEBUG:
+                logger.info('Auth Backend: Imported Main Campus user info. User creation complete.')
 
         else:
             # Error. This shouldn't ever happen.
-            model_user = None
+            login_user = None
             raise ValidationError('Error: Attempted to create user {0} but user with id already exists.'.format(uid))
 
-        return model_user
+        return login_user
 
     def user_can_authenticate(self, user):
         """
@@ -424,11 +441,19 @@ class WmuAuthBackend(object):
     Custom authentication through the WMU main campus LDAP.
     """
     def __init__(self):
+        """
+        Note: "check_credentials" value is set to False, because otherwise it will ping the LDAP server to verify
+            the "master" credentials every time this Auth Backend is called.
+
+            When set to active in settings, this backend is used every time a page needs to check permissions. So
+            enabling "check_credentials" would potentially add a lot of extra time to each page load.
+        """
         self.ldap_lib = simple_ldap_lib.SimpleLdap()
         self.ldap_lib.set_host(settings.WMU_LDAP['host'])
         self.ldap_lib.set_master_account(
             settings.WMU_LDAP['login_dn'],
             settings.WMU_LDAP['login_password'],
+            check_credentials=False,
             get_info='NONE',
         )
         self.ldap_lib.set_search_base(settings.WMU_LDAP['user_search_base'])
@@ -447,6 +472,10 @@ class WmuAuthBackend(object):
 
         # Check what format username was provided as.
         user_id = self._parse_username(username)
+
+        if user_id is None:
+            logger.info('Auth Backend: CAE user login failed.')
+            return None
 
         try:
             # Attempt to get user object.
@@ -573,82 +602,101 @@ class WmuAuthBackend(object):
         :return:
         """
         if settings.AUTH_BACKEND_DEBUG:
-            logger.info('Auth Backend: Attempting to create new user model...')
-
-        # Connect to server and pull user's full info.
-        self.ldap_lib.bind_server(get_info='NONE')
-        ldap_user = self.get_ldap_user_info(uid,
-                                            attributes=['wmuBannerID', 'wmuFirstName', 'wmuMiddleName', 'wmuLastName', 'mail', 'wmuProgramCode'],
-                                            )
-        self.ldap_lib.unbind_server()
+            logger.info('Auth Backend: Attempting to create new User model...')
 
         # Create new user.
-        model_user, created = models.User.objects.get_or_create(username=uid)
+        login_user, created = models.User.objects.get_or_create(username=uid)
 
         # Double check that user was created. If not, then duplicate user ids exist somehow. Error.
         if created:
             # Set password.
-            model_user.set_password(password)
-            model_user.save()
+            login_user.set_password(password)
+            login_user.save()
 
             # Set general (login) user values.
-            model_user.email = '{0}@wmich.edu'.format(uid)
-            model_user.first_name = ldap_user['wmuFirstName'][0].strip()
-            model_user.last_name = ldap_user['wmuLastName'][0].strip()
+            login_user.email = '{0}@wmich.edu'.format(uid)
 
             # Save model in case of error.
-            model_user.save()
+            login_user.save()
             if settings.AUTH_BACKEND_DEBUG:
-                logger.info('Auth Backend: Created user new user model {0}. Now setting groups...'.format(uid))
+                logger.info('Auth Backend: Created user new User model {0}. Now creating WmuUser model...'.format(uid))
 
-            # Set related WMU User model info.
-            try:
-                # Attempt to get related model. Just to see if it exists or not.
-                wmu_user = models.WmuUser.objects.get(bronco_net=uid)
-            except models.WmuUser.DoesNotExist:
-                # Create new related model.
-                winno = ldap_user['wmuBannerID']
-                try:
-                    first_name = ldap_user['wmuFirstName'].strip()
-                except KeyError:
-                    first_name = ''
-                try:
-                    middle_name = ldap_user['wmuMiddleName'].strip()
-                except KeyError:
-                    middle_name = ''
-                try:
-                    last_name = ldap_user['wmuLastName'].strip()
-                except KeyError:
-                    last_name = ''
-                try:
-                    official_email = ldap_user['mail'].strip()
-                except KeyError:
-                    official_email = '{0}@wmich.edu'.format(uid)
+            self.update_or_create_wmu_user_model(uid)
 
-                major = models.Major.objects.get(slug='unk')
-
-                wmu_user = models.WmuUser.objects.create(
-                    bronco_net=uid,
-                    winno=winno,
-                    first_name=first_name,
-                    middle_name=middle_name,
-                    last_name=last_name,
-                    official_email=official_email,
-                    major=major,
-                )
-
-            # Save models.
-            model_user.save()
-            wmu_user.save()
             if settings.AUTH_BACKEND_DEBUG:
                 logger.info('Auth Backend: Related WMU User model set. User creation complete.'.format(uid))
 
         else:
             # Error. This shouldn't ever happen.
-            model_user = None
-            raise ValidationError('Error: Attempted to create user {0} but user with id already exists.'.format(uid))
+            login_user = None
+            raise ValidationError(
+                'Error: Attempted to create user {0} but user with id already exists.'.format(uid))
 
-        return model_user
+        return login_user
+
+    def update_or_create_wmu_user_model(self, uid):
+        """
+        Attempts to get and update WmuUser model with given bronconet.
+        In the event that no such model exists, instead create it from scratch using ldap info from main campus.
+        :param uid: Id of user to get.
+        :return: Instance of WmuUser model.
+        """
+        # Attempt to get related model. Just to see if it exists or not. If it does exist, update if applicable.
+        try:
+            wmu_user = models.WmuUser.objects.get(bronco_net=uid)
+
+            # Update major here.
+
+        except models.WmuUser.DoesNotExist:
+            # Doesn't exist. Create new WmuUser model.
+
+            # Get win number info.
+            winno = self.get_ldap_user_attribute(uid, 'wmuBannerID')
+            if winno is None or winno == '':
+                raise ValidationError('User {0} got empty winno from Main Campus LDAP.')
+
+            # Get first name info.
+            first_name = self.get_ldap_user_attribute(uid, 'wmuFirstName')
+            if first_name is None or first_name == '':
+                first_name = self.get_backup_ldap_name(uid)
+
+            # Get middle name info.
+            middle_name = self.get_ldap_user_attribute(uid, 'wmuMiddleName')
+
+            # Get last name info.
+            last_name = self.get_ldap_user_attribute(uid, 'wmuLastName')
+            if last_name is None or last_name == '':
+                last_name = self.get_backup_ldap_name(uid)
+
+            # Get email info.
+            official_email = self.get_ldap_user_attribute(uid, 'mail')
+            if official_email is None or official_email == '':
+                official_email = '{0}@wmich.edu'.format(uid)
+
+            major = models.Major.objects.get(slug='unk')
+
+            models.WmuUser.objects.create(
+                bronco_net=uid,
+                winno=winno,
+                first_name=first_name,
+                middle_name=middle_name,
+                last_name=last_name,
+                official_email=official_email,
+                major=major,
+            )
+
+        # Attempt to update user profile.
+        user_profile = models.Profile.get_profile(uid)
+        if user_profile is None:
+            raise ValueError('Could not find profile for user {0}.'.format(uid))
+
+        # Get phone number info.
+        phone_number = self.get_ldap_user_attribute(uid, 'homePhone')
+        if phone_number is not None and phone_number != '':
+            user_profile.phone_number = PhoneNumber.from_string(phone_number)
+        user_profile.save()
+
+        return models.WmuUser.objects.get(bronco_net=uid)
 
     def user_can_authenticate(self, user):
         """
@@ -771,20 +819,72 @@ class WmuAuthBackend(object):
 
     def get_ldap_user_info(self, uid, attributes=None):
         """
-        Gets info for given user.
+        Gets attribute info for given user.
+        Can get multiple attributes at once (or all of them).
+        Returns None if any if given attribute(s) does not exist.
         :param uid: User id to search for.
         :param attributes: Attributes to search for. If not provided, then gets all attributes for user.
-        :return: None if bad user_id | Dict of user's attributes.
+        :return: None if bad user_id or attribute | Dict of user's attributes.
         """
+        # Validate vars.
         if attributes is None:
             attributes = 'ALL_ATTRIBUTES'
         elif not isinstance(attributes, list) and attributes != 'ALL_ATTRIBUTES':
             raise ValidationError('Attributes var must be a of type [list | None].')
 
-        self.ldap_lib.bind_server()
+        # Get value from server.
+        self.ldap_lib.bind_server(get_info='NONE')
         user_attributes = self.ldap_lib.search(search_filter='(uid={0})'.format(uid), attributes=attributes)
         self.ldap_lib.unbind_server()
 
+        # Check server response.
         if user_attributes is None:
-            logger.warning('Search returned no results. Double check that the uid ({0}) was correct.'.format(uid))
+            logger.warning('Search returned no results. Double check that the uid ({0}) was correct. Otherwise, one or '
+                           'more attributes did not exist for entry.'.format(uid))
         return user_attributes
+
+    def get_ldap_user_attribute(self, uid, attribute):
+        """
+        Gets a single attribute's info for given user.
+        Safer than above method to use if LDAP entries are not uniform.
+        :param uid: User id to search for.
+        :param attribute: String of attribute to search for.
+        :return: None if bad user_id or attribute | String of user attribute.
+        """
+        # Validate vars.
+        if attribute is None or not isinstance(attribute, str):
+            raise ValidationError('Attribute must be of type string.')
+        attribute = attribute.strip()
+        if attribute == '':
+            raise ValidationError('Attribute cannot be an empty string.')
+
+        # Get value from server.
+        self.ldap_lib.bind_server(get_info='NONE')
+        user_attribute = self.ldap_lib.search(search_filter='(uid={0})'.format(uid), attributes=[attribute])
+        self.ldap_lib.unbind_server()
+
+        # Check server response.
+        if user_attribute is None:
+            logger.warning('Search returned no results. Double check that the uid ({0}) was correct. Otherwise, '
+                           'attribute did not exist for entry.'.format(uid))
+        else:
+            # Format server response.
+            user_attribute = user_attribute[attribute]
+
+            # Check if is list.
+            if isinstance(user_attribute, list):
+                if len(user_attribute) == 1:
+                    # Attribute is one value. Just get value.
+                    user_attribute = user_attribute[0].strip()
+                elif len(user_attribute) > 1:
+                    # Attribute has multiple entries. Leave as is.
+                    pass
+                else:
+                    # Attribute returned as empty.
+                    user_attribute = ''
+
+            # Check if string.
+            elif isinstance(user_attribute, str):
+                user_attribute = user_attribute.strip()
+
+        return user_attribute
