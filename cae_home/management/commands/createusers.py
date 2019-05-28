bf6deaa1f9aa26b1e5fe614566c9007aed79b5df
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-05-28 17:40:44 -0400

    Implement logic for manage.py createusers method

--- cae_home/management/commands/createusers.py
+++ cae_home/management/commands/createusers.py
@@ -3,9 +3,18 @@ Command that creates users from a passed list of bronconet id's.
 For security, these users always default to inactive, and must be manually activated.
 """
 
+import re
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
+
+from settings.ldap_backends import wmu_auth
+from settings import extra_settings
+
+
+logger = extra_settings.logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -48,14 +57,70 @@ class Command(BaseCommand):
         Creates model seeds.
         """
         self.stdout.write(self.style.HTTP_INFO('\nCreate User command has been called.'))
+
+        # Initialize LDAP backend connectors.
+        cae_ldap = wmu_auth.CaeAuthBackend()
+        wmu_ldap = wmu_auth.WmuAuthBackend()
+
+        # Open file.
         file_name = kwargs['file_name']
         self.stdout.write(self.style.HTTP_INFO('Attempting to read file "{0}"...'.format(file_name)))
         file = open(file_name)
 
+        # Process each user (should be one bronco_net per line).
         for line in file:
-            get_user_model().get_or_create_user(line, '{0}@wmich.edu'.format(line), 'temppass2', inactive=True)
-            self.stdout.write(self.style.HTTP_INFO('Created user "{0}"'.format(line.strip())))
+            uid = line
 
+            # Validate that line is uid. Must be a string comprised of only letters or numbers.
+            if not isinstance(uid, str):
+                raise ValidationError('Each line should be a string of a single user\'s bronconet.')
+            elif not re.match(r'[a-zA-Z0-9]+$', uid):
+                raise ValidationError('Bronconet must be comprised of only standard numbers or letters.')
+            else:
+                uid = uid.strip().lower()
+
+            # First create user with the "dummy password". Defaults to inactive to prevent security holes.
+            login_user = get_user_model().get_or_create_user(uid, '{0}@wmich.edu'.format(uid), 'temppass2', inactive=True)
+
+            # Get CAE Center name info as a backup, in case fields are missing in Main Campus Ldap.
+            ldap_user_info = cae_ldap.get_ldap_user_info(uid, attributes=['uid', 'givenName', 'sn', ])
+            if ldap_user_info is not None:
+                login_user.first_name = ldap_user_info['givenName'][0].strip()
+                login_user.last_name = ldap_user_info['sn'][0].strip()
+            else:
+                print('User is not in CAE LDAP.')
+            login_user.save()
+            self.stdout.write(self.style.HTTP_INFO('Created user "{0}"'.format(uid.strip())))
+
+            # Check if user is part of any CAE Center groups.
+            self.stdout.write(self.style.HTTP_INFO('Checking for membership of CAE Center groups...'))
+            user_cae_groups = cae_ldap.get_ldap_user_groups(uid)
+
+            if user_cae_groups['director']:
+                login_user.groups.add(Group.objects.get(name='CAE Director'))
+                self.stdout.write(self.style.HTTP_INFO('Set director group.'))
+            if user_cae_groups['attendant']:
+                login_user.groups.add(Group.objects.get(name='CAE Attendant'))
+                self.stdout.write(self.style.HTTP_INFO('Set attendant group.'))
+            if user_cae_groups['admin']:
+                login_user.groups.add(Group.objects.get(name='CAE Admin'))
+                self.stdout.write(self.style.HTTP_INFO('Set admin group.'))
+            if user_cae_groups['programmer']:
+                login_user.groups.add(Group.objects.get(name='CAE Programmer'))
+                login_user.is_staff = True
+                self.stdout.write(self.style.HTTP_INFO('Set programmer group.'))
+
+            login_user.save()
+            self.stdout.write(self.style.HTTP_INFO('CAE Center group membership set.'))
+
+            # Set user info from main campus LDAP.
+            self.stdout.write(self.style.HTTP_INFO('Importing main campus user info...'))
+
+            # Set related WMU User model info.
+            wmu_ldap.update_or_create_wmu_user_model(uid)
+
+            self.stdout.write(self.style.HTTP_INFO('Main campus import complete. Starting next user...\n'))
+
+        # Close file.
         file.close()
         self.stdout.write(self.style.HTTP_INFO('\nUser creation complete.'))
-
