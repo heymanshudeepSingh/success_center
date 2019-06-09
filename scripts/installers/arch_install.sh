71a0b002376e07422a32b8bff5ba965cee33d313
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-06-09 13:21:49 -0400

    Update arch_install script to use better encoding type on mysql setup

--- scripts/installers/arch_install.sh
+++ scripts/installers/arch_install.sh
@@ -93,20 +93,24 @@ function main() {
         chmod g+rw -R /var/lib/mysql
 
         # Set extra database config values.
+        # Note that mysql "utf8" is apparently only encoded in 3 bytes per character. Real utf8 is 4 bytes per.
+        # To fix this, mysql "utf8mb4" was introduced that follows proper utf8 encoding standards.
+        # See https://medium.com/@adamhooper/in-mysql-never-use-utf8-use-utf8mb4-11761243e434 for more info.
         echo "" >> /etc/mysql/my.cnf
         echo "#" >> /etc/mysql/my.cnf
         echo "# Set default character set." >> /etc/mysql/my.cnf
         echo "#" >> /etc/mysql/my.cnf
         echo "[client]" >> /etc/mysql/my.cnf
-        echo "default-character-set = utf8" >> /etc/mysql/my.cnf
+        echo "default-character-set = utf8mb4" >> /etc/mysql/my.cnf
         echo "" >> /etc/mysql/my.cnf
         echo "[mysqld]" >> /etc/mysql/my.cnf
-        echo "collation_server = utf8_unicode_ci" >> /etc/mysql/my.cnf
-        echo "character_set_server = utf8" >> /etc/mysql/my.cnf
+        echo "collation_server = utf8mb4_unicode_ci" >> /etc/mysql/my.cnf
+        echo "character_set_server = utf8mb4" >> /etc/mysql/my.cnf
         echo "" >> /etc/mysql/my.cnf
         echo "[mysql]" >> /etc/mysql/my.cnf
-        echo "default-character-set = utf8" >> /etc/mysql/my.cnf
+        echo "default-character-set = utf8mb4" >> /etc/mysql/my.cnf
         echo "" >> /etc/mysql/my.cnf
+
         echo "" >> /etc/mysql/my.cnf
         echo "#" >> /etc/mysql/my.cnf
         echo "# Allow auto-completion in MySQL client." >> /etc/mysql/my.cnf
