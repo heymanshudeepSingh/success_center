57a0c14c406728700712875381e3fee88cdbad02
Brandon Rodriguez <brodriguez8774@gmail.com>
2019-10-11 16:04:54 -0400

    Correct allowed_apps import bug and partial rework for project name-casing

--- settings/allowed_apps.py
+++ settings/allowed_apps.py
@@ -92,6 +92,16 @@ ALLOWED_CAE_PROJECTS = {
 }
 
 
+
+casefolded_project_dict = {}
+# Set all project keys to be case insensitive.
+for key in ALLOWED_CAE_PROJECTS.keys():
+    casefolded_project_dict[str(key).casefold()] = ALLOWED_CAE_PROJECTS[key]
+    casefolded_project_dict[str(key).casefold()]['official_name'] = key
+
+ALLOWED_CAE_PROJECTS = casefolded_project_dict
+
+
 # Automatically populated for automatic url generation on home page. Do not edit.
 INSTALLED_CAE_PROJECTS = {}
 
@@ -105,17 +115,33 @@ debug_print('Automatically Installed Apps:')
 project_folder_list = [
     x for x in os.listdir(APP_DIR) if os.path.isdir(os.path.join(APP_DIR, x)) and not x.startswith('__')
 ]
-for project_name in project_folder_list:
+for project_folder_name in project_folder_list:
+    # Set to ignore string casing.
+    project_key_name = project_folder_name.casefold()
 
     # Check that project is defined through settings.
-    if project_name in ALLOWED_CAE_PROJECTS.keys():
-        debug_print('   {0}Included Project{1}: {2}'.format(ConsoleColors.bold_blue, ConsoleColors.reset, project_name))
+    project_defined_by_key = False
+    project_defined_by_official_name = False
+    project_official_name = ALLOWED_CAE_PROJECTS[project_key_name]['official_name']
+    if project_folder_name in ALLOWED_CAE_PROJECTS.keys():
+        project_defined_by_key = True
+    elif project_folder_name in [x['official_name'] for x in ALLOWED_CAE_PROJECTS.values()]:
+        project_defined_by_official_name = True
+
+    # If was defined in settings, proceed.
+    if project_defined_by_key or project_defined_by_official_name:
+        debug_print('   {0}Included Project{1}: {2}'.format(
+            ConsoleColors.bold_blue,
+            ConsoleColors.reset,
+            project_folder_name,
+        ))
 
         # Grab all app folders within given project.
-        project_folder = os.path.join(APP_DIR, project_name)
+        project_folder_path = os.path.join(APP_DIR, project_folder_name)
         app_folder_list = [
-            x for x in os.listdir(project_folder)
-            if os.path.isdir(os.path.join(project_folder, x)) and not x.startswith('.git') and not x.startswith('_')
+            str(x).casefold() for x in os.listdir(project_folder_path)
+            if os.path.isdir(os.path.join(project_folder_path, x)) and
+               not x.startswith('.git') and not x.startswith('_')
         ]
         included_app_list = []
         excluded_app_list = []
@@ -125,23 +151,24 @@ for project_name in project_folder_list:
 
             # Check that app is defined through settings.
             try:
-                if app_name in ALLOWED_CAE_PROJECTS[project_name]['related_apps']:
-                    app = 'apps.{0}.{1}'.format(project_name, app_name)
-                    config = ALLOWED_CAE_PROJECTS[project_name]['related_apps'][app_name].get('config')
+                # Check that app is defined through settings.
+                if app_name in ALLOWED_CAE_PROJECTS[project_key_name]['related_apps']:
+                    app = 'apps.{0}.{1}'.format(project_folder_name, app_name)
+                    config = ALLOWED_CAE_PROJECTS[project_key_name]['related_apps'][app_name].get('config')
                     if config:
                         # Add the Config to the INSTALLED_APPS
                         # We don't override 'app' because that is used for urls later
-                        new_app = 'apps.{0}.{1}.{2}'.format(project_name, app_name, config)
+                        new_app = 'apps.{0}.{1}.{2}'.format(project_folder_name, app_name, config)
                         INSTALLED_APPS.insert(1, new_app)
                     else:
                         INSTALLED_APPS.insert(1, app)
-                    INSTALLED_CAE_PROJECTS[project_name] = ALLOWED_CAE_PROJECTS[project_name]
-                    INSTALLED_CAE_PROJECTS[project_name]['related_apps'][app_name] = app
+                    INSTALLED_CAE_PROJECTS[project_key_name] = ALLOWED_CAE_PROJECTS[project_key_name]
+                    INSTALLED_CAE_PROJECTS[project_key_name]['related_apps'][app_name] = app
                     debug_print('       {0}Included App{1}: {2:<25}   {0}Url{1}: .../{3}/{2}/'.format(
                         ConsoleColors.bold_blue,
                         ConsoleColors.reset,
                         app_name,
-                        INSTALLED_CAE_PROJECTS[project_name]['url-prefix']
+                        INSTALLED_CAE_PROJECTS[project_key_name]['url-prefix']
                     ))
                     installed_app_count += 1
                     included_app_list.append(app_name)
@@ -154,21 +181,21 @@ for project_name in project_folder_list:
 
         # Print out all whitelisted but not found apps.
         delete_list = []
-        for app_name in ALLOWED_CAE_PROJECTS[project_name]['related_apps']:
+        for app_name in ALLOWED_CAE_PROJECTS[project_key_name]['related_apps']:
             if app_name not in included_app_list and app_name not in excluded_app_list:
                 debug_print('       {0}Missing App{1}:  {2}'.format(ConsoleColors.bold_yellow, ConsoleColors.reset, app_name))
                 delete_list.append(app_name)
 
         # Remove all references to whitelisted but not found apps.
         for item in delete_list:
-            del ALLOWED_CAE_PROJECTS[project_name]['related_apps'][item]
+            del ALLOWED_CAE_PROJECTS[project_key_name]['related_apps'][item]
 
         # Print out all excluded apps.
         for app_name in excluded_app_list:
             debug_print('       {0}Excluded App{1}: {2}'.format(ConsoleColors.bold_red, ConsoleColors.reset, app_name))
 
         # Add any third party apps.
-        for third_party_app in ALLOWED_CAE_PROJECTS[project_name].get('third_party_apps', []):
+        for third_party_app in ALLOWED_CAE_PROJECTS[project_key_name].get('third_party_apps', []):
             if third_party_app in INSTALLED_APPS:
                 debug_print('       Ignoring Third Party App: {0:<14}  Already Installed.'.format(third_party_app))
                 continue
@@ -181,7 +208,7 @@ for project_name in project_folder_list:
 
     else:
         # Project folder not allowed through settings.
-        excluded_project_list.append(project_name)
+        excluded_project_list.append(project_folder_name)
 
 # Print out excluded projects.
 for project_name in excluded_project_list:
