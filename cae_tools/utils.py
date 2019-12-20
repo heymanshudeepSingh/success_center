5024b28b658c7ff7c8a4f30d5fe62aa07e152948
Simar Singh <hfv6838@wmich.edu>
2019-12-20 14:58:11 -0500

    create initial emailing functions

---
+++ cae_tools/utils.py
@@ -0,0 +1,71 @@
+"""
+Utility Functions for Cae Tools.
+"""
+
+# System Imports.
+import logging
+from django.core.mail import send_mail, send_mass_mail
+
+
+def test_single_email():
+    """
+    Tests sending of email with "send_mail" function.
+    This function is acceptable when a single email is to be sent.
+    """
+
+    logging.info('Sending test email...\n')
+
+    # Compose email.
+    email_from = 'cae-programmers@wmich.edu'
+    email_to = 'cae-programmers@wmich.edu'
+    email_subject = 'Test Email from CAE Workspace Project'
+    email_message = \
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi bibendum est a nisl convallis, at laoreet' \
+        'lorem vehicula. Phasellus nulla magna, vulputate vel ex vel, suscipit convallis diam. Aenean nec velit' \
+        'velit. Cras dictum bibendum erat, et rutrum quam scelerisque in. Integer sed nunc non velit lobortis' \
+        'congue ultrices malesuada est. Aliquam efficitur id mi eget malesuada. Mauris tempor leo nec mi blandit,' \
+        'sed sagittis augue dapibus. Pellentesque sem leo, pulvinar eget tellus in, vehicula imperdiet dolor.' \
+        'Donec nec pharetra nulla. Fusce ac nulla aliquet, pellentesque diam at, dictum tortor. '
+
+    # Send email.
+    send_mail(
+        email_subject,
+        email_message,
+        email_from,
+        [email_to, ],
+        fail_silently=False,
+    )
+
+    logging.info('Email sent.\n')
+
+
+def test_mass_email():
+    """
+    Tests sending of email with "send_mass_mail" function.
+    This function is far more efficient when sending multiple emails. We are likely to use this as the default.
+    Note that, despite the name, send_mass_email can still send a single email, if desired.
+    """
+
+    logging.info('Sending test emails...\n')
+
+    # Compose email contents.
+    email_from = 'cae-programmers@wmich.edu'
+    email_to = 'cae-programmers@wmich.edu'
+    email_subject = 'Test Email from CAE Workspace Project'
+    email_1_message = \
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi bibendum est a nisl convallis, at laoreet' \
+        'lorem vehicula. Phasellus nulla magna, vulputate vel ex vel, suscipit convallis diam. Aenean nec velit' \
+        'velit. Cras dictum bibendum erat, et rutrum quam scelerisque in. Integer sed nunc non velit lobortis' \
+        'congue ultrices malesuada est. Aliquam efficitur id mi eget malesuada. Mauris tempor leo nec mi blandit,' \
+        'sed sagittis augue dapibus. Pellentesque sem leo, pulvinar eget tellus in, vehicula imperdiet dolor.' \
+        'Donec nec pharetra nulla. Fusce ac nulla aliquet, pellentesque diam at, dictum tortor. '
+    email_2_message = 'This is a test email from the CAE Center.'
+
+    # Compose emails.
+    email_1 = (email_subject, email_1_message, email_from, [email_to, ])
+    email_2 = (email_subject, email_2_message, email_from, [email_to, ])
+
+    # Send emails.
+    send_mass_mail((email_1, email_2), fail_silently=False)
+
+    logging.info('Emails sent.\n')
