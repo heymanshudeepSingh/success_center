f72f789cbefc1614c489938912b7768dd3385bca
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-02-06 17:57:25 -0500

    Update production scripts for recent script folder changes

--- scripts/production/restart_server.sh
+++ scripts/production/restart_server.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/env bash
-# Basic script to restart production server.
+###
+ # Basic script to restart production server.
+ ##
 
 
-color_reset='\033[0m'
-color_red='\033[1;31m'
+# Import utility script.
+source $(dirname $0)/utils.sh
+
+
+# Standardize current terminal path to project "scripts" directory.
+change_to_scripts_directory
 
 
 function main () {
     # Make sure we are root.
-    if [ "$USER" != "root" ]
-    then
-        echo ""
-        echo -e "${color_red}Please run script as sudo user. Terminating script.${color_reset}"
-        echo ""
-        exit 0
-    else
-        echo "Restarting Django server..."
+    check_user "root"
 
-        service nginx restart
-        service uwsgi restart
-        service daphne restart
+    echo "Restarting Django server..."
 
-        echo "Server restarted."
-        echo ""
-        echo "To run this manually, use the following commands:"
-        echo "    sudo service nginx restart"
-        echo "    sudo service uwsgi restart"
-        echo "    sudo service daphne restart"
-    fi
+    service nginx restart
+    service uwsgi restart
+    service daphne restart
+
+    echo "Server restarted."
+    echo ""
+    echo "To run this manually, use the following commands:"
+    echo "    sudo service nginx restart"
+    echo "    sudo service uwsgi restart"
+    echo "    sudo service daphne restart"
 }
+
 
 main
--- scripts/production/set_file_permissions.sh
+++ scripts/production/set_file_permissions.sh
@@ -1,81 +1,74 @@
 #!/usr/bin/env bash
-# Basic script to set (reset) project file permissions.
+###
+ # Basic script to set (reset) project file permissions.
+ ##
 
 
 # Abort on error.
 set -e
 
 
-# Global variables.
-color_reset='\033[0m'
-color_red='\033[1;31m'
-color_green='\033[1;32m'
-color_blue='\033[1;34m'
-color_cyan='\033[1;36m'
+# Import utility script.
+source $(dirname $0)/utils.sh
 
 
-# Get arguments.
-args=${@}
+# Standardize current terminal path to project "scripts" directory.
+change_to_scripts_directory
 
 
 function main () {
     # Make sure we are root.
-    if [ "$USER" != "root" ]
-    then
-        echo ""
-        echo -e "${color_red}Please run script as sudo user. Terminating script.${color_reset}"
-        echo ""
-        exit 0
-    else
-        # Default to project root, which is 2 directories up from script location.
-        directory_location="$(dirname "$0")/../.."
-        force_progress=""
+    check_user "root"
 
-        # Handle args.
-        for arg in $args
-        do
-            # Handle if "force_progress" arg was passed.
-            if [[ $arg == "force_progress" || $arg == "-f" ]]
-            then
-                force_progress=True
+    # Default to project root, which is 1 directories up from scripts folder.
+    directory_location="$(dirname "$0")/.."
+    force_progress=""
 
-            # Handle everything else. Assume is directory location.
-            elif [[ -d $arg ]]
-            then
-                directory_location=$arg
-            fi
-        done
-
-        # Change directory.
-        cd $directory_location
-
-        # Display location and give user chance to cancel.
-        echo ""
-        echo -e "${color_blue}Setting permissions for directory and all sub-folders/sub-files:${color_reset}"
-        echo "$(pwd)"
-        echo ""
-        if [[ ! $force_progress ]]
+    # Handle args.
+    for arg in $args
+    do
+        # Handle if "force_progress" arg was passed.
+        if [[ $arg == "force_progress" || $arg == "-f" ]]
         then
-            echo -e "${color_cyan}Press enter to continue or ctrl+c to cancel.${color_reset}"
-            read user_input
+            force_progress=True
+
+        # Handle everything else. Assume is directory location.
+        elif [[ -d $arg ]]
+        then
+            directory_location=$arg
         fi
+    done
 
-        echo "Setting project file ownership..."
-        chown www-data:ceas_programmers -R ./
+    # Change directory.
+    cd $directory_location
 
-        echo "Setting project file permissions..."
-        find . -type d -exec chmod u+rwx {} \;  # Set all directors to be read/write/executable by user owner.
-        find . -type d -exec chmod g+rwx {} \;  # Set all directories to be read/write/executable by group owners.
-        find . -type d -exec chmod o-rwx {} \;  # Remove directory write/executable access by other users.
-        find . -type f -exec chmod u+rw {} \;   # Set all files to be read/writeable by user owner.
-        find . -type f -exec chmod g+rw {} \;   # Set all files to be read/writeable by group owners.
-        find . -type f -exec chmod o-rwx {} \;  # Remove file write/executable access by other users.
-
-        echo ""
-        echo "Set all directories to be read/write/executable by user and group owners."
-        echo "Set all files to be read/writable by user and group owners."
-        echo "Removed all permissions for any other users."
+    # Display location and give user chance to cancel.
+    echo ""
+    echo -e "${color_blue}Setting permissions for directory and all sub-folders/sub-files:${color_reset}"
+    echo "$(pwd)"
+    echo ""
+    if [[ ! $force_progress ]]
+    then
+        echo -e "${color_cyan}Press enter to continue or ctrl+c to cancel.${color_reset}"
+        read user_input
     fi
+
+    echo "Setting project file ownership..."
+    chown www-data:ceas_programmers -R ./
+
+    echo "Setting project file permissions..."
+    find . -type d -exec chmod u+rwx {} \;  # Set all directors to be read/write/executable by user owner.
+    find . -type d -exec chmod g+rwx {} \;  # Set all directories to be read/write/executable by group owners.
+    find . -type d -exec chmod o-rwx {} \;  # Remove directory write/executable access by other users.
+    find . -type f -exec chmod u+rw {} \;   # Set all files to be read/writeable by user owner.
+    find . -type f -exec chmod g+rw {} \;   # Set all files to be read/writeable by group owners.
+    find . -type f -exec chmod o-rwx {} \;  # Remove file write/executable access by other users.
+
+    echo ""
+    echo "Set all directories to be read/write/executable by user and group owners."
+    echo "Set all files to be read/writable by user and group owners."
+    echo "Removed all permissions for any other users."
 }
+
 
 main
