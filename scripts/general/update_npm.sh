cbf2a28bce0abb66b3f7054c4118a03bfdc2e805
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-02-10 16:44:10 -0500

    Create "update_npm" script

---
+++ scripts/general/update_npm.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+###
+ # Script to update all npm dependencies.
+ ##
+
+
+# Abort on error
+set -e
+
+
+# Import utility script.
+source $(dirname $0)/../utils.sh
+
+
+# Standardize current terminal path to project "scripts" directory.
+change_to_scripts_directory
+
+
+function main () {
+    # Remove node_modules directory to start from scratch.
+    cd ..
+    rm -r ./node_modules
+    cd ./scripts/
+
+    # Update npm dependency versions.
+    echo -e "${color_blue}Automatically updating npm package dependencies to newest versions.${color_reset}"
+    ncu -u
+
+    # Install npm dependencies.
+    echo ""
+    echo ""
+    echo -e "${color_blue}Installing npm dependencies.${color_reset}"
+    npm install
+
+    # echo ""
+    # echo ""
+    # echo -e "${color_blue}Attempting to update remaining dependencies.${color_reset}"
+    # npm install -g npm-check-updates
+
+    # Attempt to fix audit issues.
+    echo ""
+    echo ""
+    echo -e "${color_blue}Attempting to correct dependency vulnerabilities.${color_reset}"
+    npm audit fix
+
+    echo ""
+    echo -e "${color_green}Npm dependencies updated.${color_reset}"
+    echo -e "${color_green}Remember to test changes by attempting to compile react.${color_reset}"
+    echo -e "${color_green}If anything fails, attempt to troubleshoot or revert version of issue dependencies before commiting.${color_reset}"
+}
+
+
+main
