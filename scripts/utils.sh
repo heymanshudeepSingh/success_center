732200d2669670967277abb4de5137a1cd04fb0b
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-03-10 16:26:10 -0400

    Reorganize utils.sh script

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -44,6 +44,154 @@ args=()
 declare -A kwargs
 
 
+#region General Helper Functions
+
+###
+ # Checks if user matches provided argument.
+ ##
+function check_user () {
+    # Check that user value was provided by first arg.
+    if [[ "$1" == "" ]]
+    then
+        echo ""
+        echo -e "${color_red}No user passed in to \"check_user\" function. Terminating script.${color_reset}"
+        echo ""
+        exit 1
+    fi
+
+    # Check that users match.
+    if [[ "$USER" != "$1" ]]
+    then
+        echo ""
+        echo -e "${color_red}Please run script as \"$1\" user. Terminating script.${color_reset}"
+        echo ""
+        exit 1
+    fi
+}
+
+
+###
+ # Checks that user does NOT match provided argument.
+ ##
+function check_not_user () {
+    # Check that user value was provided by first arg.
+    if [[ "$1" == "" ]]
+    then
+        echo ""
+        echo -e "${color_red}No user passed in to \"check_user\" function. Terminating script.${color_reset}"
+        echo ""
+        exit 1
+    fi
+
+    # Check that users do not match.
+    if [[ "$USER" == "$1" ]]
+    then
+        echo ""
+        echo -e "${color_red}Please do not run script as \"$1\" user. Terminating script.${color_reset}"
+        echo ""
+        exit 1
+    fi
+}
+
+
+###
+ # Display passed prompt and get user input.
+ # If provided, first arg is used as output text.
+ # Return true on "yes" or false otherwise.
+ ##
+function user_confirmation () {
+
+    # Get user's input.
+    echo -e "$1 ${color_cyan}[ Yes | No ]${color_reset}"
+    read user_input
+
+    # Convert to lower case.
+    if [[ $user_input != "" ]]
+    then
+        string_to_lower $user_input
+    else
+        return_value=""
+    fi
+
+    if [[ "$return_value" = "yes" ]] || [[ "$return_value" = "ye" ]] || [[ "$return_value" = "y" ]]
+    then
+        return_value=true
+    else
+        return_value=false
+    fi
+}
+
+
+###
+ # Converts string to upper case.
+ ##
+function string_to_upper () {
+    if [[ $1 != "" ]]
+    then
+        return_value="$(echo $1 | tr [':lower:'] [':upper:'])"
+    else
+        echo -e "${color_red}No value was passed. Cannot convert to upper case.${color_reset}"
+        return_value=""
+    fi
+}
+
+
+###
+ # Converts string to lower case.
+ ##
+function string_to_lower () {
+    if [[ $1 != "" ]]
+    then
+        return_value="$(echo $1 | tr [':upper:'] [':lower:'])"
+    else
+        echo -e "${color_red}No value was passed. Cannot convert to lower case.${color_reset}"
+        return_value=""
+    fi
+}
+
+#endregion General Helper Functions
+
+
+#region Directory Management Functions
+
+###
+ # Change to location of script's directory to the same directory script resides in..
+ # Can help make logic more consistent, if location of terminal matters.
+ ##
+function normalize_directory () {
+    cd "$(dirname "$0")"
+}
+
+
+###
+ # Change location of script's directory to the project's top level "scripts" folder.
+ # Can help make logic more consistent, if location of terminal matters.
+ ##
+function change_to_scripts_directory () {
+    # First normalize directory, so it doesn't matter where we called from in terminal..
+    normalize_directory
+
+    # Recursively cd upwards until we find the "scripts" directory.
+    # If "scripts" directory doesn't exist, then cd until we find the root directory.
+    while [[ "${PWD##*/}" != "scripts" ]]
+    do
+        # Check if we found the root directory.
+        if [[ "$(pwd)" == "/" ]]
+        then
+            echo ""
+            echo -e "${color_red}Could not find \"scripts\" directory. Terminating script.${color_reset}"
+            echo ""
+            exit 1
+        fi
+        cd ..
+    done
+}
+
+#endregion Directory Management Functions
+
+
+#region Arg Parsing/Management Functions
+
 ###
  # Processes all passed script args/kwargs.
  #
@@ -150,116 +298,6 @@ function display_args () {
 
 
 ###
- # Change to location of script's directory to the same directory script resides in..
- # Can help make logic more consistent, if location of terminal matters.
- ##
-function normalize_directory () {
-    cd "$(dirname "$0")"
-}
-
-
-###
- # Change location of script's directory to the project's top level "scripts" folder.
- # Can help make logic more consistent, if location of terminal matters.
- ##
-function change_to_scripts_directory () {
-    # First normalize directory, so it doesn't matter where we called from in terminal..
-    normalize_directory
-
-    # Recursively cd upwards until we find the "scripts" directory.
-    # If "scripts" directory doesn't exist, then cd until we find the root directory.
-    while [[ "${PWD##*/}" != "scripts" ]]
-    do
-        # Check if we found the root directory.
-        if [[ "$(pwd)" == "/" ]]
-        then
-            echo ""
-            echo -e "${color_red}Could not find \"scripts\" directory. Terminating script.${color_reset}"
-            echo ""
-            exit 1
-        fi
-        cd ..
-    done
-}
-
-
-###
- # Checks if user matches provided argument.
- ##
-function check_user () {
-    # Check that user value was provided by first arg.
-    if [[ "$1" == "" ]]
-    then
-        echo ""
-        echo -e "${color_red}No user passed in to \"check_user\" function. Terminating script.${color_reset}"
-        echo ""
-        exit 1
-    fi
-
-    # Check that users match.
-    if [[ "$USER" != "$1" ]]
-    then
-        echo ""
-        echo -e "${color_red}Please run script as \"$1\" user. Terminating script.${color_reset}"
-        echo ""
-        exit 1
-    fi
-}
-
-
-###
- # Checks that user does NOT match provided argument.
- ##
-function check_not_user () {
-    # Check that user value was provided by first arg.
-    if [[ "$1" == "" ]]
-    then
-        echo ""
-        echo -e "${color_red}No user passed in to \"check_user\" function. Terminating script.${color_reset}"
-        echo ""
-        exit 1
-    fi
-
-    # Check that users do not match.
-    if [[ "$USER" == "$1" ]]
-    then
-        echo ""
-        echo -e "${color_red}Please do not run script as \"$1\" user. Terminating script.${color_reset}"
-        echo ""
-        exit 1
-    fi
-}
-
-
-###
- # Display passed prompt and get user input.
- # If provided, first arg is used as output text.
- # Return true on "yes" or false otherwise.
- ##
-function user_confirmation () {
-
-    # Get user's input.
-    echo -e "$1 ${color_cyan}[ Yes | No ]${color_reset}"
-    read user_input
-
-    # Convert to lower case.
-    if [[ $user_input != "" ]]
-    then
-        string_to_lower $user_input
-    else
-        return_value=""
-    fi
-
-    if [[ "$return_value" = "yes" ]] || [[ "$return_value" = "ye" ]] || [[ "$return_value" = "y" ]]
-    then
-        return_value=true
-    else
-        return_value=false
-    fi
-}
-
-
-###
  # Checks for help flags of "-h" or "--help".
  # If present, sets "help_flag" variable to true.
  ##
@@ -286,33 +324,7 @@ function check_for_help_flags () {
     new_args=""
 }
 
-
-###
- # Converts string to upper case.
- ##
-function string_to_upper () {
-    if [[ $1 != "" ]]
-    then
-        return_value="$(echo $1 | tr [':lower:'] [':upper:'])"
-    else
-        echo -e "${color_red}No value was passed. Cannot convert to upper case.${color_reset}"
-        return_value=""
-    fi
-}
-
-
-###
- # Converts string to lower case.
- ##
-function string_to_lower () {
-    if [[ $1 != "" ]]
-    then
-        return_value="$(echo $1 | tr [':upper:'] [':lower:'])"
-    else
-        echo -e "${color_red}No value was passed. Cannot convert to lower case.${color_reset}"
-        return_value=""
-    fi
-}
+#endregion Arg Parsing/Management Functions
 
 
 # Process all args.
