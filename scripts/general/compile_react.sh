a31da741215a230a1820f827f6f2f97aac35878a
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-06-10 08:47:32 -0400

    Add (somewhat naive) ability for compile_js script to merge files

--- scripts/general/compile_react.sh
+++ scripts/general/compile_react.sh
@@ -28,6 +28,12 @@ function main () {
     echo "   * watch - Watches for changes."
     echo ""
 
+    compile_react
+    merge_js
+}
+
+
+function compile_react() {
     # Variables.
     command=""
     watch="browserify"
@@ -44,7 +50,7 @@ function main () {
         # Check if actually a directory.
         if [[ -d $dir ]]
         then
-            # Check if folder name ends in "css".
+            # Check if folder name ends in "js".
             if [[ $dir == *"/js" ]]
             then
                 # Check that react directory exists.
@@ -61,7 +67,7 @@ function main () {
                             filename=$(basename "${file%.*}")
 
                             # Create command.
-                            new_command="npx $watch -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] $file -o $js_dir/$filename.js"
+                            new_command="npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file} -o ${js_dir}/${filename}.js"
 
                             # Check if previous command exists.
                             if [[ $command == "" ]]
@@ -90,11 +96,84 @@ function main () {
 
     # Display command to user.
     echo ""
-    echo "Running command:"
+    echo -e "${color_blue}Compiling React:${color_reset}"
     echo "${command[@]}"
     echo ""
     "${command[@]}"
     echo ""
+}
+
+
+function merge_js() {
+    # Variables.
+    command=""
+    watch="browserify"
+
+    # Determine command format from passed args.
+    if [[ ${args[@]} =~ "watch" ]]
+    then
+        watch="watchify -v"
+    fi
+
+    # Determine directories to compile.
+    for dir in ../*/*/*/* ../*/*/*/*/*/*
+    do
+        # Check if actually a directory.
+        if [[ -d $dir ]]
+        then
+            # Check if folder name ends in "js".
+            if [[ $dir == *"/js" && $dir != *"third_party/js" ]]
+            then
+                # Merge JS files in directory.
+                file_set=""
+                dir_basename=${dir%/*}
+                dir_basename=${dir_basename##*/}
+                echo -e "${color_blue}Merging JS Files in ${dir_basename} directory.${color_reset}"
+
+                # Add all files in directory. Ignore files in subfolders.
+                for file in $dir/*
+                do
+                    # Check that item is a file.
+                    if [[ -f $file ]]
+                    then
+                        if [[ $file_set == "" ]]
+                        then
+                            # Handle empty string.
+                            file_set="${file}"
+                        else
+                            # Append to existing fileset.
+                            file_set+=" ${file}"
+                        fi
+                    fi
+                done
+
+                # Check that files were found to compile.
+                if [[ $file_set != "" ]]
+                then
+                    # Create directory.
+                    if [[ ! -d ${dir}/dist ]]
+                    then
+                        mkdir ${dir}/dist
+                    fi
+
+                    # Create command.
+                    new_command="npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file_set} -o ${dir}/dist/${dir_basename}.js"
+                    command=(npx concurrently "$new_command")
+
+
+                    # Display and run command.
+                    echo $(pwd)
+                    echo ""
+                    echo -e "${color_blue}Merging JS:${color_reset}"
+                    echo "${command[@]}"
+                    echo ""
+                    "${command[@]}"
+                    echo ""
+                fi
+            fi
+        fi
+    done
+
 }
 
 
