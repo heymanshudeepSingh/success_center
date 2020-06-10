c33d20a4b06bca2c26f54621f664ef3246c871f2
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-06-10 10:05:46 -0400

    Refactor compile_js script to properly handle watching files

--- scripts/general/compile_react.sh
+++ scripts/general/compile_react.sh
@@ -28,8 +28,86 @@ function main () {
     echo "   * watch - Watches for changes."
     echo ""
 
-    compile_react
-    merge_js
+    compile_command=""
+    merge_command=""
+
+    # Determine directories to compile.
+    for dir in ../*/*/*/* ../*/*/*/*/*/*
+    do
+        # Check if actually a directory.
+        if [[ -d $dir ]]
+        then
+            # Check if folder name ends in "js".
+            if [[ $dir == *"/js" ]]
+            then
+                # Get compile command.
+                compile_react
+                if [[ $compile_command == "" ]]
+                then
+                    # Does not exist. Create.
+                    compile_command=("$return_value")
+                else
+                    # Previous command exists. Append.
+                    compile_command+=(" $return_value")
+                fi
+
+                # Get merge command.
+                merge_js
+                # Check if previous command exists.
+                if [[ $merge_command == "" ]]
+                then
+                    # Does not exist. Create.
+                    merge_command=("$return_value")
+                else
+                    # Previous command exists. Append.
+                    merge_command+=(" $return_value")
+                fi
+            fi
+        fi
+    done
+
+    # Check that command is going to compile anything.
+    if [[ $compile_command != "" || $merge_command != "" ]]
+    then
+        echo ""
+        echo -e "${color_blue}Installing npm depenencies.${color_reset}"
+        cd ..
+        npm install
+        cd ./scripts/
+    fi
+
+    # Execute depending on provided "watch" arg.
+    if [[ ${args[@]} =~ "watch" ]]
+    then
+        # Watch provided by user.
+        full_command=(npx concurrently "${compile_command[@]}" "${merge_command[@]}")
+
+        echo ""
+        echo -e "${color_blue}Compiling React and Merging JavaScript:${color_reset}"
+        echo "${full_command[@]}"
+        echo ""
+        "${full_command[@]}"
+        echo ""
+
+    else
+        # Watch not provided by user.
+
+        compile_command=(npx concurrently "${compile_command[@]}")
+        echo ""
+        echo -e "${color_blue}Compiling React Code:${color_reset}"
+        echo "${compile_command[@]}"
+        "${compile_command[@]}"
+        echo ""
+
+        merge_command=(npx concurrently "${merge_command[@]}")
+        echo ""
+        echo -e "${color_blue}Merging JavaScript Files:${color_reset}"
+        echo "${merge_command[@]}"
+        echo ""
+        "${merge_command[@]}"
+        echo ""
+    fi
+
 }
 
 
@@ -44,63 +122,37 @@ function compile_react() {
         watch="watchify -v"
     fi
 
-    # Determine directories to compile.
-    for dir in ../*/*/*/* ../*/*/*/*/*/*
-    do
-        # Check if actually a directory.
-        if [[ -d $dir ]]
-        then
-            # Check if folder name ends in "js".
-            if [[ $dir == *"/js" ]]
+    # Check that react directory exists.
+    if [[ -d $dir/react ]]
+    then
+        js_dir=$dir
+        # Loop through all files in react subfolder
+        for file in $dir/react/*
+        do
+            # Check if actually a file.
+            if [[ -f $file ]]
             then
-                # Check that react directory exists.
-                if [[ -d $dir/react ]]
+                dir_basename=${dir%/*}
+                dir_basename=${dir_basename##*/}
+                echo -e "${color_blue}${dir_basename}: Found React files to compile.${color_reset}"
+
+                # Get base file name.
+                filename=$(basename "${file%.*}")
+
+                # Create command.
+                if [[ $command == "" ]]
                 then
-                    js_dir=$dir
-                    # Loop through all files in react subfolder
-                    for file in $dir/react/*
-                    do
-                        # Check if actually a file.
-                        if [[ -f $file ]]
-                        then
-                            # Get base file name.
-                            filename=$(basename "${file%.*}")
-
-                            # Create command.
-                            new_command="npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file} -o ${js_dir}/${filename}.js"
-
-                            # Check if previous command exists.
-                            if [[ $command == "" ]]
-                            then
-                                # Does not exist. Create.
-                                command=(npx concurrently "$new_command")
-                            else
-                                # Previous command exists. Append.
-                                command+=(" $new_command")
-                            fi
-                        fi
-                    done
+                    # Does not exist. Create.
+                    command=("npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file} -o ${js_dir}/${filename}.js")
+                else
+                    # Previous command exists. Append.
+                    command+=(" npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file} -o ${js_dir}/${filename}.js")
                 fi
             fi
-        fi
-    done
-
-    # Check that command is going to compile anything.
-    if [[ $command != "" ]]
-    then
-        echo -e "${color_blue}Installing npm depenencies.${color_reset}"
-        cd ..
-        npm install
-        cd ./scripts/
+        done
     fi
 
-    # Display command to user.
-    echo ""
-    echo -e "${color_blue}Compiling React:${color_reset}"
-    echo "${command[@]}"
-    echo ""
-    "${command[@]}"
-    echo ""
+    return_value=$command
 }
 
 
@@ -115,65 +167,90 @@ function merge_js() {
         watch="watchify -v"
     fi
 
-    # Determine directories to compile.
-    for dir in ../*/*/*/* ../*/*/*/*/*/*
-    do
-        # Check if actually a directory.
-        if [[ -d $dir ]]
-        then
-            # Check if folder name ends in "js".
-            if [[ $dir == *"/js" && $dir != *"third_party/js" ]]
+    # Check if folder name ends in "js".
+    if [[ $dir == *"/js" && $dir != *"third_party/js" ]]
+    then
+        # Merge JS files in directory.
+        file_set=""
+        dir_basename=${dir%/*}
+        dir_basename=${dir_basename##*/}
+        echo -e "${color_blue}${dir_basename}: Found JS files to merge.${color_reset}"
+
+        # Add all files in react directory.
+        # Annoyingly, we have to check these separately to prevent occassional nonsensical compile errors.
+        for file in $dir/react/*
+        do
+            # Check that item is a file.
+            if [[ -f $file ]]
             then
-                # Merge JS files in directory.
-                file_set=""
-                dir_basename=${dir%/*}
-                dir_basename=${dir_basename##*/}
-                echo -e "${color_blue}Merging JS Files in ${dir_basename} directory.${color_reset}"
-
-                # Add all files in directory. Ignore files in subfolders.
-                for file in $dir/*
-                do
-                    # Check that item is a file.
-                    if [[ -f $file ]]
-                    then
-                        if [[ $file_set == "" ]]
-                        then
-                            # Handle empty string.
-                            file_set="${file}"
-                        else
-                            # Append to existing fileset.
-                            file_set+=" ${file}"
-                        fi
-                    fi
-                done
-
-                # Check that files were found to compile.
-                if [[ $file_set != "" ]]
+                # Append file to file_set string.
+                if [[ $file_set == "" ]]
                 then
-                    # Create directory.
-                    if [[ ! -d ${dir}/dist ]]
-                    then
-                        mkdir ${dir}/dist
-                    fi
-
-                    # Create command.
-                    new_command="npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file_set} -o ${dir}/dist/${dir_basename}.js"
-                    command=(npx concurrently "$new_command")
-
-
-                    # Display and run command.
-                    echo $(pwd)
-                    echo ""
-                    echo -e "${color_blue}Merging JS:${color_reset}"
-                    echo "${command[@]}"
-                    echo ""
-                    "${command[@]}"
-                    echo ""
+                    # Handle empty string.
+                    file_set="${file}"
+                else
+                    # Append to existing fileset.
+                    file_set+=" ${file}"
                 fi
             fi
-        fi
-    done
+        done
 
+        # Add all files in directory. Ignore files in subfolders.
+        for file in $dir/*
+        do
+            # Check that item is a file.
+            if [[ -f $file ]]
+            then
+                # Get file path info.
+                file_name=${file##*/}
+                file_dir=${file%/*}
+                file_path=${file_dir}/react/${file_name}
+
+                # Check if react version of file exists.
+                if [[ -d ${dir}/react && -f ${file_path} ]]
+                then
+                    # React version exists. Already added so we can safely ignore file.
+                    file=""
+                fi
+
+                # Check that file should be added.
+                if [[ $file != "" ]]
+                then
+                    # Append file to file_set string.
+                    if [[ $file_set == "" ]]
+                    then
+                        # Handle empty string.
+                        file_set="${file}"
+                    else
+                        # Append to existing fileset.
+                        file_set+=" ${file}"
+                    fi
+                fi
+            fi
+        done
+
+        # Check that files were found to compile.
+        if [[ $file_set != "" ]]
+        then
+            # Create directory.
+            if [[ ! -d ${dir}/dist ]]
+            then
+                mkdir ${dir}/dist
+            fi
+
+            # Create command.
+            new_command="npx ${watch} -t [ babelify --presets [@babel/preset-env @babel/preset-react] ] ${file_set} -o ${dir}/dist/${dir_basename}.js"
+
+            if [[ $command == "" ]]
+            then
+                command=("$new_command")
+            else
+                command+=(" $new_command")
+            fi
+        fi
+    fi
+
+    return_value=$command
 }
 
 
