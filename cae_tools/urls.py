bd50c9f99b27fbc2256a29ce12ebb2002267f9c2
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-06-10 22:58:15 -0400

    Split "css examples" page up into subpages
    For both maintainability and legibility.

--- cae_tools/urls.py
+++ cae_tools/urls.py
@@ -9,8 +9,21 @@ from . import views
 
 app_name = 'cae_tools'
 urlpatterns = [
+    # CSS Example urls.
+    path('css_examples/alerts', views.css_alerts, name='css_alerts'),
+    path('css_examples/articles', views.css_articles, name='css_articles'),
+    path('css_examples/buttons', views.css_buttons, name='css_buttons'),
+    path('css_examples/forms', views.css_forms, name='css_forms'),
     path('css_examples/page_tabbing', views.css_page_tabbing, name='css_page_tabbing'),
+    path('css_examples/panels', views.css_panels, name='css_panels'),
+    path('css_examples/status_messages', views.css_status_messages, name='css_status_messages'),
+    path('css_examples/tables', views.css_tables, name='css_tables'),
+    path('css_examples/text_highlighting', views.css_text_highlighting, name='css_text_highlighting'),
     path('css_examples/', views.css_examples, name='css_examples'),
+
+    # Color Tool urls.
     path('color/', views.color_tool, name='color_tool'),
+
+    # Home url.
     path('', views.index, name='index'),
 ]
--- cae_tools/views.py
+++ cae_tools/views.py
@@ -18,9 +18,42 @@ def index(request):
     return redirect('cae_tools:color_tool')
 
 
+#region CSS Example Views
+
 def css_examples(request):
     """
-    Displays examples of custom HTML/CSS layout and stylings used in site.
+    Main page for examples of custom HTML/CSS layout and stylings provided by CAE Workspace.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_example.html', {})
+
+
+def css_alerts(request):
+    """
+    Displays examples of custom HTML/CSS layout for "alert" elements.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/alerts.html', {})
+
+
+def css_articles(request):
+    """
+    Displays examples of custom HTML/CSS layout for "article" elements.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/articles.html', {})
+
+
+def css_buttons(request):
+    """
+    Displays examples of custom HTML/CSS layout for "button" elements.
+    """
+    return TemplateResponse(request, 'cae_tools/css_examples/buttons.html', {})
+
+
+def css_forms(request):
+    """
+    Displays examples of custom HTML/CSS layout for "forms" elements.
     """
     # Get example forms.
     form = forms.ExampleForm()
@@ -28,7 +61,7 @@ def css_examples(request):
         form = forms.ExampleForm(request.POST)
 
     # Render template to user.
-    return TemplateResponse(request, 'cae_tools/css_example.html', {
+    return TemplateResponse(request, 'cae_tools/css_examples/forms.html', {
         'form': form,
     })
 
@@ -37,7 +70,42 @@ def css_page_tabbing(request):
     """
     Displays examples of custom HTML/CSS layout for "page tabbing" elements.
     """
+    # Render template to user.
     return TemplateResponse(request, 'cae_tools/css_examples/page_tabbing.html', {})
+
+
+def css_panels(request):
+    """
+    Displays examples of custom HTML/CSS layout for "panel" elements.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/panels.html', {})
+
+
+def css_status_messages(request):
+    """
+    Displays examples of custom HTML/CSS layout for "status message" elements.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/status_messages.html', {})
+
+
+def css_tables(request):
+    """
+    Displays examples of custom HTML/CSS layout for "table" elements.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/tables.html', {})
+
+
+def css_text_highlighting(request):
+    """
+    Displays examples of custom HTML/CSS layout for "text highlight" elements.
+    """
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/text_highlighting.html', {})
+
+#endregion CSS Example Views
 
 
 def color_tool(request):
