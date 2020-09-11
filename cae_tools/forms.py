2545de4848c5c3f11d8fb5958c78247d3980f328
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-09-10 23:43:55 -0400

    Add much better documentation for CSS Examples of forms

--- cae_tools/forms.py
+++ cae_tools/forms.py
@@ -6,15 +6,192 @@ Forms for CAE Tools app.
 from django import forms
 
 # User Imports.
-from cae_home.forms import DatePickerWidget, DateTimePickerWidget, TimePickerWidget
+from cae_home import forms as cae_home_forms
 
 
-class ExampleForm(forms.Form):
+# Create choices tuple. Used for select/choice fields down below.
+CHOICES = (
+    (1, 'Choice 1'),
+    (2, 'Choice 2'),
+    (3, 'Choice 3'),
+    (4, 'Choice 4'),
+    (5, 'Choice 5'),
+)
+
+
+class BaseExampleForm(forms.Form):
     """
     An example form, used in the css examples page.
     Should not actually submit any data.
+
+    Has a few randomly chosen fields, to show how a standard, basic form may look.
     """
     name = forms.CharField()
-    date_widget = forms.DateField(widget=DatePickerWidget)
-    time_widget = forms.TimeField(widget=TimePickerWidget)
+    date_widget = forms.DateField(widget=cae_home_forms.DatePickerWidget)
+    time_widget = forms.TimeField(widget=cae_home_forms.DatePickerWidget)
     check_me = forms.BooleanField(required=False)
+
+
+class DefaultFieldExampleForm_TextFields(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has the most common text fields.
+    """
+    char_field_example = forms.CharField()
+    text_field_example = forms.CharField(widget=forms.Textarea)
+
+
+class DefaultFieldExampleForm_TextFieldsUncommon(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has all less common text fields.
+    """
+    regex_field_example = forms.RegexField(r'[a-zA-Z0-9]')  # Checks for standard letters and numbers.
+    email_field_example = forms.EmailField()
+    slug_field_example = forms.SlugField()
+    url_field_example = forms.URLField()
+
+
+class DefaultFieldExampleForm_NumberFields(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has numeric-related fields.
+    """
+    integer_field_example = forms.IntegerField()
+    decimal_field_example = forms.DecimalField()
+    float_field_example = forms.FloatField()
+
+
+class DefaultFieldExampleForm_NumberFieldsAdjusted(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has more user-friendly numeric-related fields.
+    """
+    integer_field_example = forms.IntegerField()
+    decimal_field_example = forms.DecimalField(decimal_places=2, max_digits=4)
+    float_field_example = forms.FloatField(min_value=-1, max_value=1, widget=forms.NumberInput(attrs={'step': '0.1'}))
+
+
+class DefaultFieldExampleForm_DateFields(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has date/time related fields.
+    """
+    date_field_example = forms.DateField()
+    datetime_field_example = forms.DateTimeField()
+    time_field_example = forms.TimeField()
+
+
+class DefaultFieldExampleForm_DateFieldsAdjusted(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has very minorly more user-friendly date/time related fields.
+    """
+    date_field_example = forms.DateField(widget=forms.SelectDateWidget)
+    datetime_field_example = forms.DateTimeField(widget=forms.SplitDateTimeWidget)
+    time_field_example = forms.TimeField()
+
+
+class DefaultFieldExampleForm_ChoiceFields(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has choice/selection related fields.
+    """
+    choice_field_example = forms.ChoiceField(choices=CHOICES)
+    typed_choice_field_example = forms.TypedChoiceField(choices=CHOICES)
+    multiple_choice_field_example = forms.MultipleChoiceField(choices=CHOICES)
+    typed_multiple_choice_field_example = forms.TypedMultipleChoiceField(choices=CHOICES)
+
+
+class DefaultFieldExampleForm_MiscFields(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has all other fields that didn't fit in the above fieldsets.
+    """
+    boolean_field_example = forms.BooleanField()
+    null_boolean_field_example = forms.NullBooleanField()
+    duration_field_example = forms.DurationField()
+    file_field_example = forms.FileField()
+    # filepath_field_example = forms.FilePathField()
+    image_field_example = forms.ImageField()
+    generic_ip_address_field_example = forms.GenericIPAddressField()
+    uuid_field_example = forms.UUIDField()
+
+
+class CustomFieldExampleForm_DateWidgets(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has date-related custom-made form fields, provided by this workspace project.
+    """
+    reference_char_field = forms.CharField()
+    reference_text_field = forms.CharField(widget=forms.Textarea)
+    date_widget_example = forms.DateField(widget=cae_home_forms.DatePickerWidget)
+    time_widget_example = forms.TimeField(widget=cae_home_forms.TimePickerWidget)
+    date_time_widget_example = forms.DateTimeField(widget=cae_home_forms.DateTimePickerWidget())
+
+
+class CustomFieldExampleForm_SelectButtons(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has "select button"-related custom-made form fields, provided by this workspace project.
+    """
+    reference_char_field = forms.CharField()
+    reference_text_field = forms.CharField(widget=forms.Textarea)
+    select_button_widget_example = forms.MultipleChoiceField(
+        choices=CHOICES,
+        widget=cae_home_forms.SelectButtonsWidget,
+    )
+
+
+class CustomFieldExampleForm_SelectButtonsSide(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has "side select button"-related custom-made form fields, provided by this workspace project.
+    """
+    reference_char_field = forms.CharField()
+    reference_text_field = forms.CharField(widget=forms.Textarea)
+    select_button_side_widget_example = forms.ChoiceField(
+        choices=CHOICES,
+        widget=cae_home_forms.SelectButtonsSideWidget,
+    )
+
+
+class CustomFieldExampleForm_Select2(forms.Form):
+    """
+    An example form, used in the css examples page.
+    Should not actually submit any data.
+
+    Has "select2"-related custom-made form fields, provided by this workspace project.
+    """
+    reference_char_field = forms.CharField()
+    reference_text_field = forms.CharField(widget=forms.Textarea)
+    select2_widget_example = forms.ChoiceField(
+        choices=CHOICES,
+        widget=cae_home_forms.Select2Widget,
+    )
+    select2_multiple_widget_example = forms.ChoiceField(
+        choices=CHOICES,
+        widget=cae_home_forms.Select2MultipleWidget,
+    )
--- cae_tools/views.py
+++ cae_tools/views.py
@@ -53,16 +53,73 @@ def css_buttons(request):
 
 def css_forms(request):
     """
-    Displays examples of custom HTML/CSS layout for "forms" elements.
+    Displays examples of custom HTML/CSS layout for "form" elements.
     """
     # Get example forms.
-    form = forms.ExampleForm()
+    form = forms.BaseExampleForm()
     if request.method == 'POST':
-        form = forms.ExampleForm(request.POST)
+        form = forms.BaseExampleForm(request.POST)
 
     # Render template to user.
-    return TemplateResponse(request, 'cae_tools/css_examples/forms.html', {
+    return TemplateResponse(request, 'cae_tools/css_examples/forms/form.html', {
         'form': form,
+    })
+
+
+def css_forms_default_fields(request):
+    """
+    Displays examples of custom HTML/CSS layout for various default "form field" elements.
+    """
+    # Get example forms.
+    text_form = forms.DefaultFieldExampleForm_TextFields()
+    text_form_uncommon = forms.DefaultFieldExampleForm_TextFieldsUncommon()
+    number_form = forms.DefaultFieldExampleForm_NumberFields()
+    number_form_adjusted = forms.DefaultFieldExampleForm_NumberFieldsAdjusted()
+    date_form = forms.DefaultFieldExampleForm_DateFields()
+    date_form_adjusted = forms.DefaultFieldExampleForm_DateFieldsAdjusted()
+    choice_form = forms.DefaultFieldExampleForm_ChoiceFields()
+    misc_form = forms.DefaultFieldExampleForm_MiscFields()
+
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/forms/default_fields.html', {
+        'text_form': text_form,
+        'text_form_uncommon': text_form_uncommon,
+        'number_form': number_form,
+        'number_form_adjusted': number_form_adjusted,
+        'date_form': date_form,
+        'date_form_adjusted': date_form_adjusted,
+        'choice_form': choice_form,
+        'misc_form': misc_form,
+    })
+
+def css_forms_custom_fields(request):
+    """
+    Displays examples of custom HTML/CSS layout for various custom "form field" elements.
+    """
+    # Get example forms.
+    datetime_form = forms.CustomFieldExampleForm_DateWidgets()
+    select_buttons_form = forms.CustomFieldExampleForm_SelectButtons()
+    select_buttons_side_form = forms.CustomFieldExampleForm_SelectButtonsSide()
+    select2_form = forms.CustomFieldExampleForm_Select2()
+
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/forms/custom_fields.html', {
+        'datetime_form': datetime_form,
+        'select_buttons_form': select_buttons_form,
+        'select_buttons_side_form': select_buttons_side_form,
+        'select2_form': select2_form,
+    })
+
+
+def css_forms_misc(request):
+    """
+    Displays examples of custom HTML/CSS layout for all other form-related elements that don't fit in the above views.
+    """
+    # Get example forms.
+
+    # Render template to user.
+    return TemplateResponse(request, 'cae_tools/css_examples/forms/misc.html', {
+
     })
 
 
