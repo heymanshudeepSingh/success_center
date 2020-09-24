5999c80049158cb3b380a3caf572cc3a13b140c9
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-09-24 14:51:12 -0400

    Remove some leftover js debugging output

--- cae_home/static/cae_home/js/sub_nav.js
+++ cae_home/static/cae_home/js/sub_nav.js
@@ -6,8 +6,6 @@
 // Wait for full page load.
 $(document).ready(function() {
     var sub_nav_container =  $('header .sub-nav')[0];
-    console.log(sub_nav_container);
-    console.log('Children: ' + $(sub_nav_container).children().length);
 
     // Check if element contains any children. If so, display. Otherwise keep hidden.
     if ($(sub_nav_container).children().length > 0) {
