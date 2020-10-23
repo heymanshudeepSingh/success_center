ca415cb7af71e288f56fafd57c1b5dda1b264010
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-10-23 07:42:42 -0400

    Correct fixture command for software models

--- cae_home/management/commands/fixtures/cae.py
+++ cae_home/management/commands/fixtures/cae.py
@@ -2,13 +2,19 @@
 Fixture Imports for "CAE Center" related Core Models.
 """
 
+# System Imports.
+from django.core.management import call_command
+from sys import stdout
+
 
 def import_model_fixtures(style):
     """
     Calls individual fixture methods.
     """
-    # stdout.write(style.HTTP_NOT_MODIFIED('SEEDING CAE Model Group.\n'))
+    stdout.write(style.HTTP_NOT_MODIFIED('IMPORTING FIXTURES for User Model Group.\n'))
     create_assets(style)
+    create_software(style)
+    create_software_detail(style)
 
 
 def create_assets(style, display_output=False):
@@ -23,13 +29,19 @@ def create_software(style, display_output=False):
     """
     Imports fixtures for Software models.
     """
-    # Nothing here yet.
-    pass
+    # Load preset fixtures.
+    call_command('loaddata', 'production_models/software')
+
+    if display_output and style is not None:
+        stdout.write('Imported fixtures for ' + style.SQL_FIELD('Software') + ' models.\n')
 
 
 def create_software_detail(style, display_output=False):
     """
     Imports fixtures for Software Detail models.
     """
-    # Nothing here yet.
-    pass
+    # Load preset fixtures.
+    call_command('loaddata', 'production_models/software_detail')
+
+    if display_output and style is not None:
+        stdout.write('Imported fixtures for ' + style.SQL_FIELD('Software Detail') + ' models.\n')
