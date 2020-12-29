326e8302319b0670683492ce0d6beaf9e4c6855b
Brandon Rodriguez <brodriguez8774@gmail.com>
2020-12-29 06:20:30 -0500

    Create manage.py command for updating GroupManagement models

---
+++ cae_home/management/commands/update_group_membership.py
@@ -0,0 +1,22 @@
+"""
+Command to update GroupMembership models for all existing users.
+"""
+
+# System Imports.
+from django.conf import settings
+from django.core.management import call_command
+from django.core.management.base import BaseCommand, CommandError
+
+# User Class Imports.
+from cae_home.models.user import check_all_group_memberships
+
+
+class Command(BaseCommand):
+    help = 'Updates GroupMembership models for all existing users.'
+
+    def handle(self, *args, **kwargs):
+        """
+        The logic of the command.
+        """
+        # Run function from models.py file.
+        check_all_group_memberships()
