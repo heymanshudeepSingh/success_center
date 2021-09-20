1733983483dda05199da2adbd50591f2aaf6761e
Brandon Rodriguez <brodriguez8774@gmail.com>
2021-09-20 11:10:58 -0400

    Create class mixin version of custom GroupRequired function decorator

--- cae_home/decorators.py
+++ cae_home/decorators.py
@@ -39,6 +39,8 @@ def group_required(*required_groups):
     Limits view access based on user group.
     To access view, user must be part of one or more groups provided.
     Logic from https://codereview.stackexchange.com/questions/57073/django-custom-decorator-for-user-group-check
+
+    Note: If you update this, make sure to also update below GroupRequiredMixin class.
     """
     def check_group(view_func):
         @wraps(view_func)
@@ -88,3 +90,48 @@ def _validate_group(group):
             raise Group.DoesNotExist(
                 'Invalid Group name of "{0}" provided. Could not find corresponding group.'.format(group),
             )
+
+
+class GroupRequiredMixin:
+    """
+    Class mixin version of above group_required() function.
+    Works the same, just allows logic to apply to classes as well.
+
+    Note: If you update this, make sure to also update above group_required decorator function.
+    """
+    required_user_auth_groups = None
+
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Override class dispatch to check for user login and user group membership.
+        """
+        # Get current User.
+        user = request.user
+
+        # Check that user is authenticated.
+        if not user.is_authenticated:
+            messages.warning(request, 'Please log in to view the page.')
+            return redirect_to_login(request.path)
+
+        # Check that provided values are Group models. If not, convert to such.
+        required_group_set = []
+        if self.required_user_auth_groups:
+            for passed_group in self.required_user_auth_groups:
+                if isinstance(passed_group, list) or isinstance(passed_group, tuple):
+                    # Is list of groups. Handle accordingly.
+                    for inner_passed_group in passed_group:
+                        required_group_set.append(_validate_group(inner_passed_group))
+                else:
+                    # Is single value. Handle accordingly.
+                    required_group_set.append(_validate_group(passed_group))
+        else:
+            raise ValueError(
+                'GroupRequiredMixin is called, but no groups provided. Please populate class the ' +
+                '"required_user_auth_groups" value with desired groups.'
+            )
+
+        # Check that user is either superuser, or part of provided groups.
+        if user.is_superuser or bool(user.groups.filter(name__in=required_group_set).exists()):
+            return super().dispatch(request, *args, **kwargs)
+        else:
+            raise PermissionDenied
