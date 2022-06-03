678321b7c1fe27c2304f05db99730026a75027ac
affan moyeed <affan.moyeed@gmail.com>
2022-06-02 23:45:25 -0400

    added  proper seeds for wmu class

--- cae_home/management/commands/seeders/wmu.py
+++ cae_home/management/commands/seeders/wmu.py
@@ -7,6 +7,13 @@ Note that seeder methods will always call fixture methods first. Then attempt se
 # System Imports.
 from sys import stdout
 
+import faker
+from faker import Faker
+from django.utils.text import slugify
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from random import randint
+from cae_home.models.wmu import Department, WmuClass
 # User Class Imports.
 from cae_home.management.commands.fixtures import wmu as wmu_fixtures
 
@@ -21,14 +28,66 @@ def generate_model_seeds(style, model_count):
     create_rooms(style)
     create_majors(style)
     create_semester_dates(style)
-    create_wmu_classes(style)
+    create_wmu_classes(style, model_count)
 
 
-def create_wmu_classes(style):
+def create_wmu_classes(style, model_count):
     """
     Create WMU Class models.
     """
     wmu_fixtures.create_wmu_classes(style)
+    # Create random data generator.
+    faker_factory = Faker()
+
+    # Count number of models already created.
+    pre_initialized_count = len(WmuClass.objects.all())
+
+    # Get all related models.
+    departments = Department.objects.all()
+
+    # Generate models equal to model count.
+    total_fail_count = 0
+    for i in range(model_count - pre_initialized_count):
+        fail_count = 0
+        try_create_model = True
+
+        # Loop attempt until 3 fails or model is created.
+        # Model creation may fail due to field unique requirement.
+        while try_create_model:
+
+            # Get Room.
+            index = randint(0, len(departments) - 1)
+            department = departments[index]
+
+            # Generate classroom codes.
+            code = faker_factory.bothify(text='???#####')
+
+            # Attempt to create model seed.
+            try:
+                WmuClass.objects.create(
+                    department=department,
+                    code=code,
+                    title=faker_factory.sentence(nb_words=5, variable_nb_words=True),
+                    description=faker_factory.paragraph(),
+                    slug=slugify(code)
+                )
+
+                try_create_model = False
+            except (ValidationError, IntegrityError):
+                # Seed generation failed. Nothing can be done about this without removing the random generation aspect.
+                # If we want that, we should use fixtures instead.
+                fail_count += 1
+
+                # If failed 3 times, give up model creation and move on to next model, to prevent infinite loops.
+                if fail_count > 2:
+                    try_create_model = False
+                    total_fail_count += 1
+
+    # Output if model instances failed to generate.
+    if total_fail_count > 0:
+        stdout.write(style.WARNING(
+            'Failed to generate {0}/{1} WmuClass seed instances.\n'.format(total_fail_count, model_count)
+        ))
 
     stdout.write('Populated ' + style.SQL_FIELD('WmuClass') + ' models.\n')
 
