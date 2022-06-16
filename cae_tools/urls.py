4383f661e510041cc0115cb2ca6a69bedd56f092
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-06-15 22:28:35 -0400

    Correct url name

--- cae_tools/urls.py
+++ cae_tools/urls.py
@@ -10,7 +10,7 @@ from . import views
 app_name = 'cae_tools'
 urlpatterns = [
     # LDAP Utility Pages
-    path('ldap/search/', views.ldap_utility, name='ldap_utility'),
+    path('padl/search/', views.ldap_utility, name='padl_utility'),
     path('password_reset/', views.cae_password_reset, name='password_reset'),
     path('reset_password/', views.cae_password_reset, name='reset_password'),
 
