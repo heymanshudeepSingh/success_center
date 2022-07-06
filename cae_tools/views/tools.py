9330b6820f96fece719b79c420541da32a271a03
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-07-06 01:03:05 -0400

    Add depreciation warnings and messages for color tool page

--- cae_tools/views/tools.py
+++ cae_tools/views/tools.py
@@ -3,6 +3,7 @@ Tool views for CAE Tools app.
 """
 
 # System Imports.
+from django.contrib import messages
 from django.template.response import TemplateResponse
 
 # User Imports.
@@ -12,4 +13,16 @@ def color_tool(request):
     """
     Color tool.
     """
+    # Add depreciated warning messages.
+    messages.warning(request, 'This page is no longer maintained, and is broken as of 2021.')
+    messages.warning(
+        request,
+        (
+            'Adobe has a team of full-time professionals that maintain something better than we could possibly make. '
+            'Use this page to reference "official" wmu colors. Please see https://color.adobe.com for a much better '
+            'version of what this page was trying to be.'
+        ),
+    )
+    messages.warning(request, 'Please see https://color.adobe.com')
+
     return TemplateResponse(request, 'cae_tools/color_tool.html', {})
