89f73825a5921658472604a099824336f12d3b47
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-07-29 13:39:32 -0400

    Allow select2 to search by text or id field matches

--- cae_home/static/cae_home/js/select2.js
+++ cae_home/static/cae_home/js/select2.js
@@ -1,18 +1,55 @@
 /**
  * Detects and initializes html elements with the select2 class.
+ *
+ * For "matcher" logic, see https://stackoverflow.com/questions/31019609/filter-by-id-as-well-as-text-in-select2
  */
 
 
+/**
+ * Custom function to modify default matching.
+ */
+window.cae_functions['select2__match_custom'] = function(params, data) {
+    // If there are no search terms, return all of the data
+    if ($.trim(params.term) === '') {
+      return data;
+    }
+
+    // Do not display the item if there is no 'text' property.
+    if (typeof data.text === 'undefined') {
+      return null;
+    }
+
+    // Search by value.
+    if (data.text.indexOf(params.term) > -1) {
+      return data;
+    }
+
+    // Search by id.
+    if (data.id.indexOf(params.term) > -1) {
+      return data;
+    }
+
+    // Return `null` if the term should not be displayed
+    return null;
+}
+
+
 $(document).ready(function() {
+
     // Basic select2 form element.
-    $('.form-widget-select2').select2();
+    $('.form-widget-select2').select2({
+        matcher: window.cae_functions['select2__match_custom'],
+    });
 
     // Basic select2 form element, but with ability for user to create new entries in real time.
     $('.form-widget-select2-with-tagging').select2({
         tags: true,
-        selectOnClose: true
+        selectOnClose: true,
+        matcher: window.cae_functions['select2__match_custom'],
     });
 
     // Multi-choice select2 form element.
-    $('.form-widget-select2-multiple').select2();
+    $('.form-widget-select2-multiple').select2({
+        matcher: window.cae_functions['select2__match_custom'],
+    });
 });
