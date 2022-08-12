b686cdfcb945f608fad9c9ce67aa92048fcdb87f
Brandon Rodriguez <brodriguez8774@gmail.com>
2022-08-11 21:42:04 -0400

    Improve/update handling of breadcrumb + subnav elements

--- cae_home/static/cae_home/js/breadcrumb_nav.js
+++ cae_home/static/cae_home/js/breadcrumb_nav.js
@@ -10,14 +10,26 @@ $(document).ready(function() {
     window.cae_vars['breadcrumbNav__container'] = $("#breadcrumb-nav");
     window.cae_vars['breadcrumb_element'] = $("#breadcrumb-nav ul");
 
+    // Check if breadcrumb element is populated.
     if ($(window.cae_vars['breadcrumb_element']).children().length > 0) {
+
+        // First, dynamically add separator elements into breadcrumb.
+
         // Add separator symbols between breadcrumb items.
-        $("<li>&nbsp;&gt;&nbsp;</li>").insertAfter($(window.cae_vars['breadcrumb_element'].children()));
+        $("<li class='separator'>&nbsp;&lt;&nbsp;</li>").insertAfter($(window.cae_vars['breadcrumb_element'].children()));
 
         // Remove last separator, as it's trailing and doesn't separate anything useful.
         $(window.cae_vars['breadcrumb_element']).children().last().remove();
 
         // Display breadcrumb container to user.
         window.cae_vars['breadcrumbNav__container'].css("display", "flex");
+
+        // Dynamically move/format BreadCrumb nav element on page load.
+
+        // Check if page has h1 element (it should, but we make sure).
+        let h1_element = $('h1');
+        if (h1_element.length > 0) {
+            window.cae_vars['breadcrumbNav__container'].detach().insertAfter(h1_element);
+        }
     }
 });
--- cae_home/static/cae_home/js/sub_nav.js
+++ cae_home/static/cae_home/js/sub_nav.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     window.cae_vars['nav__subNavContainer'] = $('header .sub-nav')[0];
 
     // Check if element contains any children. If so, display. Otherwise keep hidden.
-    if ($(window.cae_vars['nav__subNavContainer']).children().length > 0) {
+    if ($(window.cae_vars['nav__subNavContainer']).children().children().length > 0) {
         $(window.cae_vars['nav__subNavContainer']).css('display', '');
     }
 });
