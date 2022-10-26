66cc30eb0f169377ddabef91636607ffe126584c
Devin Ragotzy <devin.ragotzy@gmail.com>
2022-10-26 15:05:10 -0400

    Just to test stuff out... REMOVE

--- workspace/ldap_backends/wmu_auth/adv_backend.py
+++ workspace/ldap_backends/wmu_auth/adv_backend.py
@@ -501,10 +501,14 @@ class AdvisingAuthBackend(AbstractLDAPBackend):
         wmu_user = models.WmuUser.objects.get(bronco_net=uid)
         login_user = wmu_user.userintermediary.user
 
-        # If this fails there is another problem in the DB
-        # The migration script (laravel -> django) adds multiple majors per day a user is logged in
-        # this should guard that only the last active major is found and deactivated
-        model_relationship = wmu_user.wmuusermajorrelationship_set.get(wmu_user=wmu_user, major=major, is_active=True)
+        try:
+            # If this fails there is another problem in the DB
+            # The migration script (laravel -> django) adds multiple majors per day a user is logged in
+            # this should guard that only the last active major is found and deactivated
+            model_relationship = wmu_user.wmuusermajorrelationship_set.get(wmu_user=wmu_user, major=major, is_active=True)
+        except Exception as e:
+            logger.warn(f'FOOOOBAR\n{wmu_user.__dict__}\n\n{major.__dict__}')
+            return
 
         # Set deactivation date.
         # If associated (login) User model exists and is inactive, use last login date.
